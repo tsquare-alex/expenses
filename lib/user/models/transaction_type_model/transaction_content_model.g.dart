4e6d0fd58b0ae76cbe660f37ae67e3aa76aadddc
Mohamed Salama <01550526487m@gmail.com>
2023-12-14 17:13:00 +0200

    transaction edit

---
+++ lib/user/models/transaction_type_model/transaction_content_model.g.dart
@@ -0,0 +1,42 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'transaction_content_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class TransactionContentModelAdapter
+    extends TypeAdapter<TransactionContentModel> {
+  @override
+  final int typeId = 1;
+
+  @override
+  TransactionContentModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return TransactionContentModel(
+      name: fields[0] as String?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, TransactionContentModel obj) {
+    writer
+      ..writeByte(1)
+      ..writeByte(0)
+      ..write(obj.name);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TransactionContentModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}

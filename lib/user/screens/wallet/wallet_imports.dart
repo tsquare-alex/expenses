da739189ce640592a6ff0fa488099bef6df24896
ahmed <ao1922302@gmail.com>
2023-12-20 12:51:44 -0800

    imports changes

--- lib/user/screens/wallet/wallet_imports.dart
+++ lib/user/screens/wallet/wallet_imports.dart
@@ -4,7 +4,6 @@ import 'package:expenses/general/utilities/routers/RouterImports.gr.dart';
 import 'package:expenses/general/widgets/MyText.dart';
 import 'package:expenses/user/screens/wallet/data/cubit/wallet_cubit/wallet_cubit.dart';
 import 'package:expenses/user/screens/wallet/data/cubit/wallet_cubit/wallet_state.dart';
-import 'package:expenses/user/screens/wallet/widgets/aleret_dialog_body.dart';
 import 'package:expenses/user/screens/wallet/widgets/wallet_body.dart';
 import 'package:expenses/user/screens/wallet/widgets/wallet_detalis.dart';
 import 'package:flutter/material.dart';

4db559c3b254c4b119d522749553dfb188b13d06
ahmed <ao1922302@gmail.com>
2023-12-22 23:05:24 -0800

    changes

--- lib/user/screens/wallet/wallet_imports.dart
+++ lib/user/screens/wallet/wallet_imports.dart
@@ -2,7 +2,6 @@ import 'package:auto_route/auto_route.dart';
 import 'package:expenses/general/constants/MyColors.dart';
 import 'package:expenses/general/utilities/routers/RouterImports.gr.dart';
 import 'package:expenses/general/widgets/DefaultButton.dart';
-import 'package:expenses/general/widgets/MyText.dart';
 import 'package:expenses/user/screens/wallet/data/cubit/wallet_cubit/wallet_cubit.dart';
 import 'package:expenses/user/screens/wallet/data/cubit/wallet_cubit/wallet_state.dart';
 import 'package:expenses/user/screens/wallet/widgets/wallet_body.dart';

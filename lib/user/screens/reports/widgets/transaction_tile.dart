d1e1e185aefe5e3326dec089cfc8b56cc1649dff
Ahmed Shahawy <ahmed.shahawy@yahoo.com>
2023-12-24 20:41:49 +0200

    - fix transaction name in report screen

--- lib/user/screens/reports/widgets/transaction_tile.dart
+++ lib/user/screens/reports/widgets/transaction_tile.dart
@@ -15,7 +15,7 @@ class TransactionTile extends StatelessWidget {
         titleAlignment: ListTileTitleAlignment.center,
         contentPadding: EdgeInsets.only(left: 25.r, right: 16.r),
         title: Text(
-          '${transaction.transactionType!.name!} ${transaction.transactionContent!.name!}',
+          "${transaction.transactionName == "الالتزامات" || transaction.transactionName == "التسوق والشراء" ? transaction.transactionType!.name! : transaction.transactionName == "الاهداف المالية المستهدفة" ? transaction.targetType?.name : transaction.transactionName == "المعاملات النقدية" ? transaction.cashTransactionType?.name : ""}",
           style: TextStyle(
             fontWeight: FontWeight.bold,
             fontSize: 13.sp,

7d860ae28a4a6ba89aa4f1d4135048ebc07b3eab
Mohamed Salama <01550526487m@gmail.com>
2024-02-04 21:00:13 +0200

    show transaction on budget

--- lib/user/screens/add_transaction/add_transaction_data.dart
+++ lib/user/screens/add_transaction/add_transaction_data.dart
@@ -743,7 +743,7 @@ class AddTransactionData {
             notify: notifyCubit.state.data,
             putReminderInWallet: remainderCubit.state.data,
           );
-          var total = double.parse(totalController.text);
+          var total = double.parse(initialValueController.text);
           if (total <= selectedWalletModel!.totalBalance!) {
             var walletBox = Hive.box<WalletModel>(walletDatabaseBox);
             var currencyBox = Hive.box<CurrencyModel>("currencyBox");
